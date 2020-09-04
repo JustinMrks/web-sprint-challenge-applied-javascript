a045fa9d28246f4426f9e36699bdfb8308128556
justinmrks <justinmarks19@gmail.com>
2020-09-04 11:45:30 -0500

    finished the sprint challenge and hate objects again

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,23 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+import axios from 'axios';
+
+const topicsDiv = document.querySelector('div.topics')
+
+function createTabs(data){
+    data.forEach(item => {
+        const newTab = document.createElement('div')
+        newTab.classList.add('tab')
+        newTab.textContent = item
+        topicsDiv.appendChild(newTab)
+    })
+}
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(info =>{
+        createTabs(info.data.topics)
+    })
+    .catch(err =>{
+        console.log(err)
+    })
